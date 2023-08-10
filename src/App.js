8299121694f7166739ac34851f2421dd57e58820
Milana <milana.zaborovska@gmail.com>
2023-08-10 23:02:37 +0100

    header complete unresponsive

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./header/Navbar";
+import HeaderBody from "./header/HeaderBody";
+
+function App() {
+  return (
+    <div className="header">
+      <Navbar/>
+      <HeaderBody/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/header/Navbar.jsx
@@ -0,0 +1,10 @@
+import './header.css';
+
+export default function Navbar(){
+    return(
+        <div className="navbar__background">
+            <h2>Logo</h2>
+            <span> </span>
+        </div>
+    )
+}
